948e926cbac4be06077a30410763289b513bfe1a
Fatma Akcan <fatma.akcan.dev@gmail.com>
2026-03-07 15:08:54 +0300

    Function alıştırması yapıldı.

---
+++ function-demo.py
@@ -0,0 +1,38 @@
+def displayWord (word, times):
+    print(word * times)
+
+displayWord("Hello ", 5)
+
+print("*********" * 10)
+
+def add (*args):
+    return list(args)
+
+print(add(1, 2, 3, 4, 5,"Merhaba")) 
+
+print("*********" * 10)
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+def primeNum(num1,num2):
+    for num in range(num1,num2):
+        if num > 1:
+            for i in range(2,num):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)
+    print("There is no prime number between", num1, "and", num2)            
+print("Prime numbers between", num1, "and", num2, "are:")    
+primeNum(num1,num2)
+
+print("*********" * 10) 
+
+def findDivider(num):
+    for i in range(1,num+1):
+        if num%i==0:
+            print(i)
+num=int(input("Enter a number:"))
+print(num,"'s dividers are:")
+findDivider(num)
