eabfe801d9eb1aafb919a58679181d5c33301ced
Fatma Akcan <fatma.akcan.dev@gmail.com>
2026-04-07 00:11:48 +0300

    try-except mini examples have been solved.

---
+++ errors-demo.py
@@ -0,0 +1,65 @@
+list=["1","2","5a","10b","abc","10","50"]
+
+#finding the numbers in the list
+
+for x in list:
+    try:
+        result=int(x)
+        print(result)
+    except ValueError:
+        continue
+
+print("*****"*10)
+
+#ValueError example with the input from the user
+
+while True:
+    number=input("number: ")
+    if number=="q":
+        print("Exiting...")
+        break
+    
+    try:
+        result=int(number)
+    except ValueError:
+        print("Invalid value!")
+        continue
+
+print("*****"*10)
+
+#Mini password validatio example
+
+turkish_characters="şçğüöıİ"
+
+password=input("password: ")
+
+for i in password:
+    if i in turkish_characters:
+        raise TypeError("Password cannot include Turkish characters!")
+    else:
+        pass
+print("Valid password :) ")
+
+print("*****"*10)
+
+#factorial function example
+
+def factorial(x):
+    x=int(x)
+    
+    if x<0:
+        raise ValueError("Negative value")
+    
+    result=1
+    
+    for i in range(1,x+1):
+        result*=i
+    return result
+
+for x in [5,10,20,-3,"10a"]:
+    try:
+        y=factorial(x)
+    except ValueError as err:
+        print(err)
+        continue
+    print(y)   
